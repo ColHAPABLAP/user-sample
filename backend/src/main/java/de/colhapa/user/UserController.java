defe35150ff1668aab307c2cc99cba7843d42829
ColHAPABLAP <Michael.Ruetz@t-online.de>
2017-04-08 13:05:20 +0200

    Added logging for getUser method.

--- backend/src/main/java/de/colhapa/user/UserController.java
+++ backend/src/main/java/de/colhapa/user/UserController.java
@@ -1,6 +1,8 @@
 package de.colhapa.user;
 
 import de.colhapa.user.persistence.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,11 +13,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/user", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class UserController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
 	@Autowired
 	private UserService userService;
 
 	@RequestMapping(method = RequestMethod.GET, path = "/{userId}")
 	public User getUser(@PathVariable("userId") String userId) {
+		LOGGER.info("Calling getUser()");
+
 		return userService.getUser(userId);
 	}
 
