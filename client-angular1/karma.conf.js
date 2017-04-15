d90f37b51e2971b9f71b41be7e3156470d310caa
ColHAPABLAP <Michael.Ruetz@t-online.de>
2017-04-15 17:28:12 +0200

    Added new AngularJS client setup.

---
+++ client-angular1/karma.conf.js
@@ -0,0 +1,40 @@
+//jshint strict: false
+module.exports = function (config) {
+    config.set({
+
+        basePath: './app',
+
+        files: [
+            'bower_components/angular/angular.js',
+            'bower_components/angular-route/angular-route.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            'components/**/*.js',
+            'view*/**/*.js'
+        ],
+
+        autoWatch: true,
+
+        frameworks: ['jasmine'],
+
+        browsers: ['Chrome'],
+
+        phantomjsLauncher: {
+            // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
+            exitOnResourceError: true
+        },
+
+        plugins: [
+            'karma-phantomjs-launcher',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine',
+            'karma-junit-reporter'
+        ],
+
+        junitReporter: {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+        }
+
+    });
+};
