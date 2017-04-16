27935d1b3e98607d798b24cc2e6e6e2249e8df61
ColHAPABLAP <Michael.Ruetz@t-online.de>
2017-04-16 12:40:46 +0200

    Added basic view to Angular client.

--- client-angular1/app/app.js
+++ client-angular1/app/app.js
@@ -1,6 +1,16 @@
 'use strict';
 
-angular.module('app', [
-    'ngRoute',
-    'ui.bootstrap'
-]);
+angular
+    .module('app', [
+        'ngRoute',
+        'ui.bootstrap',
+        'app.userView'
+    ])
+    .config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+        // Behaviour change for hashbang since 1.6: https://github.com/angular/angular.js/commit/aa077e81129c740041438688dff2e8d20c3d7b52
+        // use for old style hashbang (just '#')
+        $locationProvider.hashPrefix('');
+
+        // default routing
+        $routeProvider.otherwise({redirectTo: '/user'});
+    }]);
