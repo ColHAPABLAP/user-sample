7537874a16daeb10d764438a4b35ad42dd52286d
ColHAPABLAP <Michael.Ruetz@t-online.de>
2017-08-18 23:48:33 +0200

    Upgraded Spring version and added spring actuator

--- backend/src/main/java/de/colhapa/BackendApplication.java
+++ backend/src/main/java/de/colhapa/BackendApplication.java
@@ -2,8 +2,6 @@ package de.colhapa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
 //@EntityScan(basePackageClasses = {BackendApplication.class, Jsr310JpaConverters.class})
